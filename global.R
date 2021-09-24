6f737f57ad949e2cedd99b371fe72f9c4fb0c2ad
tateemma <tateemma@outlook.com>
2021-09-24 13:53:08 +0200

    Update global.R

--- global.R
+++ global.R
@@ -3,11 +3,11 @@ library(shinythemes)
 library(ggplot2)
 library(dplyr)
 
-source("ui.R")
-source("server.R")
-
 ghana_data = read.csv("Ghana data shiny.csv", row.names=NULL, sep=";")
 sn_data = read.csv("Senegal Market Prices.csv")
 bf_data = read.csv("Burkina Faso Market Prices.csv")
+
+source("ui.R")
+source("server.R")
 
 shinyApp(ui = ui, server = server)
